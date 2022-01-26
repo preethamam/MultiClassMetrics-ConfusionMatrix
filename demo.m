904c138ecad4104b83561e80925cc78d6dd2b566
Preetham Manjunatha <preethamam@gmail.com>
2022-01-25 21:05:28 -0800

    First push with all dependencies

---
+++ demo.m
@@ -0,0 +1,22 @@
+clear; close all; clc;
+
+confmat = [298     2       1     0       1       1     3      1       1       0;
+     0     293     7     4       1       0     5      2       0       0;
+     1     3      263    0       8       0     0      3       0       2;
+     1     5       0     261     4       0     3      2       0       1;
+     0     0       10    0     254       3     0     10       2       1;
+     0     4       1     1       4       300   0      1       0       0;
+     1     3       2     0       0       0     264    0       7       1;
+     3     5       3     1       7       1     0      289     1       0;
+     0     1       3     13      1       0     11     1       289     0;
+     0     6       0     1       6       1     2      1       4       304];
+ 
+ 
+ 
+ 
+ [Precision, Recall, Accuracy, Specificity, F1score] = multiclass_metrics_2(confmat)
+ [Result,ReferenceResult] = multiclass_metrics(confmat)
+%  Result
+
+ 
+
---
+++ multiclass_metrics_common.m
@@ -0,0 +1,90 @@
+function [Precision, Recall, Accuracy, Specificity, F1score] = multiclass_metrics_common(confmat) 
+
+%%***********************************************************************%
+%*                           Panorama Cropper                           *%
+%*        Crops a fully stitched panorama with black/white background.  *%
+%*                                                                      *%
+%* Code author: Preetham Manjunatha                                     *%
+%* Github link: https://github.com/preethamam
+%* Date: 11/24/2021                                                     *%
+%************************************************************************%
+%
+%************************************************************************%
+%
+% Usage: croppedImage = panoramaCropper(input, stitchedImage)
+% Inputs: input.canvas_color            - Background canvas color: 'black'
+%                                         | 'white'
+%         input.blackRange              - Threshold value > 0 and 
+%                                         < some number close to 0 (e.g, 5, 10).     
+%         input.whiteRange              - Threshold value close to 255  
+%                                         (e.g, 254, 250, 236,...)
+%         input.showCropBoundingBox     - Display the cropping bounding box
+% 
+% Outputs: croppedImage - Final cropped image
+
+% Array initialization
+N = size(confmat,1);
+
+Precision = zeros(1,N);
+Recall = zeros(1,N);
+Specificity = zeros(1,N);
+Accuracy = zeros(1,N);
+F1score = zeros(1,N);
+
+
+if size(confmat,1) > 2
+
+     for i = 1:size(confmat,1)
+
+        TP = confmat(i,i);
+        FN = sum(confmat(i,:))-confmat(i,i);
+        FP = sum(confmat(:,i))-confmat(i,i);
+        TN = sum(confmat(:))-TP -FP-FN;
+
+        Precision(:,i)   = TP / (TP+FP); % positive predictive value (PPV)
+        Recall(:,i)      = TP / (TP+FN); % true positive rate (TPR), sensitivity
+        if ((TN / (TN+FP)) > 1)
+            Specificity(:,i) = 1;
+        elseif ((TN / (TN+FP)) < 0)
+            Specificity(:,i) = 0;
+        else
+            Specificity(:,i) = TN / (TN+FP); % (SPC) or true negative rate
+        end
+        Accuracy(:,i)    = (TP)/(TP+TN+FP+FN); % Accuracy
+        F1score(:,i)     = (2*TP) /(2*TP + FP + FN);
+     end
+    
+        % Remove junks         
+        stats = [Precision', Recall', F1score', Accuracy', Specificity'];
+        stats(any(isinf(stats),2),:) = [];
+        stats(any(isnan(stats),2),:) = [];
+        N = size(stats,1);
+        
+        % Compute averages
+        Accuracy  = sum(stats(:,4));
+        Precision = mean(stats(:,1));
+        Recall    = mean(stats(:,2));
+        Specificity = mean(Specificity);
+        F1score = mean(stats(:,3));
+else
+        TP = confmat(1, 1);
+        FP = confmat(2, 1);
+        FN = confmat(1, 2);
+        TN = confmat(2,2);
+
+        Precision = TP / (TP+FP); % positive predictive value (PPV)
+        Recall    = TP / (TP+FN); % true positive rate (TPR), sensitivity
+        if ((TN / (TN+FP)) > 1)
+            Specificity = 1;
+        elseif ((TN / (TN+FP)) < 0)
+            Specificity = 0;
+        else
+            Specificity = TN / (TN+FP); % (SPC) or true negative rate
+        end
+        Accuracy = (TP+TN)/(TP+TN+FP+FN); % Accuracy
+        F1score  = 2*TP /(2*TP + FP + FN);
+    
+end
+
+ 
+
